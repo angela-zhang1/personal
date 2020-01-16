c747fdf18298ce153a05b50f230f2786db49bf5c
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2020-01-15 20:13:36 -0500

    Create leaguesearch.java

---
+++ leaguesearch.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+//so maybe i got a bit tired of scrolling through the list of all the champions
+//so i classified them any which way i could want and here they all are
+
+public class leaguesearch {
+	public static void main (String[] args) {
+		
+	}
+	public static String lane () {}
+	static class champion () {
+		public int c; //cost
+		public int type; //i don't even know what this is but like what they're specialized in
+		public int lane; //0 = jung, 1 = top, 2 = mid, 3 = bot, 4 = sup
+		public String name; //should be self explanatory
+	
+	}
+}
